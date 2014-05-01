e9de2ee06323280ddc4efde854b939a225a1ec1c
Oliver Moran <oliver.moran@gmail.com>
2014-05-02 00:05:32 +0100

    update examples with edit page and forced priority

--- example.js
+++ example.js
@@ -7,6 +7,8 @@ var password = "password";
 // instantiate a bot
 var bot = new MediaWiki.Bot();
 
+// requests are queued and executed in order, throttled to 10/minute by default
+
 bot.login(user, password).complete(function (user) {
     console.log("Logged in!");
 });
@@ -33,9 +35,17 @@ bot.history("Wikipedia", 10).complete(function (title, history) {
     console.log(title);
     console.log(history.length);
     
-    bot.revision(history[1].revid).complete(function (title, text, date) {
+    // 'true' at the end of any request means that the requst will be prioritised
+    // i.e. it will be put to the top of the request queue
+    bot.revision(history[1].revid).complete(function (title, text, date, true) {
         console.log(title);
         console.log(text.length);
         console.log(date);
     });
+});
+
+bot.edit("Wikipedia:Sandbox", (new Date()).toString(), "replaced the page contents with the current time").complete(function (title, revision, date) {
+    console.log(title);
+    console.log(revision);
+    console.log(date.toString());
 });
